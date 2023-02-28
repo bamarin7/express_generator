328d43d949c91303b5bb5604425687f031859baf
Brian Marin Silva <marinbrian.as@gmail.com>
2023-02-27 19:19:19 -0600

    Adding Sessions pt 1

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -33,11 +35,21 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('1234-5678-9009-8765'));
+// app.use(cookieParser('1234-5678-9009-8765'));
+
+app.use(session({
+  name: 'session-id',
+  secret: '1234-5678-9009-8765',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
 
 // Setting up authentication
 function auth(req, res, next) {
-  if (!req.signedCookies.user) {
+  console.log(req.session);
+
+  if (!req.session.user) {
     const authHeader = req.headers.authorization;
     if(!authHeader) {
       const err = new Error('You are not authenticated!');
@@ -50,7 +62,7 @@ function auth(req, res, next) {
     const user = auth[0];
     const pass = auth[1];
     if (user === 'admin' && pass === 'password') {
-      res.cookie('user', 'admin', {signed: true});
+      req.session.user = 'admin';
       return next(); // authorized
     } else {
       const err = new Error('You are not authenticated!');
@@ -59,7 +71,7 @@ function auth(req, res, next) {
       return next(err);
     }
   } else {
-    if (req.signedCookies.user === 'admin') {
+    if (req.session.user === 'admin') {
       return next();
     } else {
       const err = new Error('You are not authenticated!');
